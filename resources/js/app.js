eb96a7a0e02843a2ed07feab0e9783bc6925532b
PabloMarcial99 <17031116@itcelaya.edu.mx>
2022-07-19 18:03:32 -0500

    Commit para mandar a repositorio

---
+++ resources/js/app.js
@@ -0,0 +1,31 @@
+import "./bootstrap";
+import "../css/app.css";
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css ";
+import "primeicons/primeicons.css";
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/inertia-vue3";
+import { InertiaProgress } from "@inertiajs/progress";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import PrimeVue from "primevue/config";
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .use(PrimeVue)
+            .mount(el);
+    },
+});
+
+InertiaProgress.init({ color: "#4B5563" });
